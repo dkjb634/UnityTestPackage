1e51bfaeb02f06e779d68b6e84b974d5d35f9711
Dmitrii.Kazantsev <dmitry.kazantsev@jetbrains.com>
2023-09-21 15:21:02 +0300

    I can commit

--- qq.cs
+++ qq.cs
@@ -5,5 +5,6 @@ namespace DefaultNamespace
     public class qq : MonoBehaviour
     {
         //It would dissapear soon.ssssdssdsddsd
+        //whether the package isa submodule oficially or not
     }
 }
